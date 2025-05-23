88a19808f0851b9a903a52e0d5d23b29821f993f
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-05-23 16:15:33 +0300

    no message

--- Sources/Java/JavaObject.swift
+++ Sources/Java/JavaObject.swift
@@ -10,15 +10,4 @@ open class JavaObject: JavaRepresentable {
     public required init(holder: JavaReference) {
         self.holder = holder
     }
-    /*
-    @_nonoverride
-    public convenience init(environment: JNIEnvironment) {
-        let this = try! Self.dynamicJavaNewObjectInstance(in: environment)
-        self.init(this: this, environment: environment)
-    }
-    
-    open func toString() -> String {
-        return try! dynamicJavaMethodCall(methodName: "toString", resultType: String.self)
-    }
-    */
 }
